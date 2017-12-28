0e115df42496c0bfb63a5d7e857385c14e7a3c2a
Erick Pires <pires@erickpires.com>
2017-12-28 13:13:17 -0200

    Parsing struct's and typedef's.

--- struct_printer.c
+++ struct_printer.c
@@ -73,9 +73,16 @@ typedef enum {
     TK_ID = 256,
     TK_NUM,
     TK_STRUCT,
+    TK_UNION,
     TK_ENUM,
     TK_TYPEDEF,
     TK_EOF,
+} token_type_t;
+
+typedef struct {
+    token_type_t token_type;
+    char* token_str;
+    size_t len;
 } token_t;
 
 bool ignored(char c) {
@@ -92,7 +99,8 @@ bool is_alpha(char c) {
         (c >= 'A' && c <= 'Z');
 }
 
-token_t next_token(char* data, char* token_str) {
+static char* last_location;
+token_t next_token(char* data) {
     static int current_index = 0;
 
     bool should_ignore;
@@ -105,7 +113,8 @@ token_t next_token(char* data, char* token_str) {
             should_ignore = true;
         }
         // Line comment
-        if(data[current_index] == '/' &&data[current_index + 1] == '/') {
+        if((data[current_index] == '/' &&data[current_index + 1] == '/') ||
+            data[current_index] == '#') {
             current_index += 2;
             while(data[current_index] != '\n' && data[current_index] != '\0') {
                 current_index++;
@@ -127,8 +136,15 @@ token_t next_token(char* data, char* token_str) {
         }
     } while(should_ignore);
 
+    last_location = data + current_index;
+
+    token_t result;
     char current_char = data[current_index];
-    if(current_char == '\0') { return TK_EOF; }
+
+    if(current_char == '\0') {
+        result.token_type = TK_EOF;
+        return result;
+    }
 
     if(current_char == '{' ||
        current_char == '}' ||
@@ -136,25 +152,38 @@ token_t next_token(char* data, char* token_str) {
        current_char == ';' ||
        current_char == '[' ||
        current_char == ']' ||
+       current_char == '(' ||
+       current_char == ')' ||
        current_char == ',') {
+
         current_index++;
-        return current_char;
+        result.token_type = current_char;
+        return result;
     }
 
     char* read_ptr = data + current_index;
     if(strstr(read_ptr, "struct") == read_ptr) {
         current_index += strlen("struct");
-        return TK_STRUCT;
+        result.token_type = TK_STRUCT;
+        return result;
+    }
+
+    if(strstr(read_ptr, "union") == read_ptr) {
+        current_index += strlen("union");
+        result.token_type = TK_UNION;
+        return result;
     }
 
     if(strstr(read_ptr, "typedef") == read_ptr) {
         current_index += strlen("typedef");
-        return TK_TYPEDEF;
+        result.token_type = TK_TYPEDEF;
+        return result;
     }
 
     if(strstr(read_ptr, "enum") == read_ptr) {
         current_index += strlen("enum");
-        return TK_ENUM;
+        result.token_type = TK_ENUM;
+        return result;
     }
 
     if(is_alpha(current_char) || current_char == '_') {
@@ -165,43 +194,168 @@ token_t next_token(char* data, char* token_str) {
                 is_num(data[current_index]));
         size_t len = data + current_index - read_ptr;
 
-        strncpy(token_str, read_ptr, len);
-        token_str[len] = '\0';
+        result.token_str = read_ptr;
+        result.len = len;
+        result.token_type = TK_ID;
 
-        return TK_ID;
+        return result;
     }
 
-    read_ptr[10] = '\0';
-    fprintf(stderr, "Unidentified token at:\n%s", read_ptr);
+    fprintf(stderr, "Unidentified token at:\n%.*s", 10, last_location);
     exit(2);
+}
+
+void fprint_token(FILE* file, token_t token) {
+    switch(token.token_type) {
+    case TK_TYPEDEF :
+        fprintf(file, "TYPEDEF\n");
+        break;
+
+    case TK_STRUCT :
+        fprintf(file, "STRUCT\n");
+        break;
+
+    case TK_UNION :
+        fprintf(file, "UNION\n");
+        break;
+
+    case TK_ENUM :
+        fprintf(file, "ENUM\n");
+        break;
+
+    case TK_ID :
+        fprintf(file, "ID: %.*s\n", (int) token.len, token.token_str);
+        break;
+
+    default:
+        fprintf(file, "%c\n", token.token_type);
+    }
+}
+
+token_t token_from_type(token_type_t type) {
+    token_t result;
+    result.token_type = type;
+
+    return result;
+}
+
+void expect(token_type_t expected, token_t value) {
+    if(expected != value.token_type) {
+        token_t expected_token = token_from_type(expected);
+
+        fprintf(stderr, "Expected : ");
+        fprint_token(stderr, expected_token);
+        fprintf(stderr, "Got: ");
+        fprint_token(stderr, value);
+
+        last_location[10] = '\0';
+        fprintf(stderr, "At: %s\n", last_location);
+        exit(3);
+    }
+}
+
+void parse_decl_or_struct_or_union(char* data, bool first_token_already_consumed) {
+    token_t token;
+    token = next_token(data);
+
+    if(token.token_type == '{') {
+        do {
+            token = next_token(data);
+        } while(token.token_type != '}');
+
+        token = next_token(data);
+        expect(';', token);
+    } else {
+        do {
+            token = next_token(data);
+        } while(token.token_type != ';');
+    }
+}
+
+void parse_declaration(char* data, bool first_token_already_consumed) {
+    token_t token;
+    do {
+        token = next_token(data);
+        if(token.token_type == TK_ID) {
+            printf("Field: %.*s\n", (int) token.len, token.token_str);
+        }
+    } while(token.token_type != ';');
+}
+
+void parse_struct_or_union(char* data) {
+    printf("In\n");
+    // char tmp[101];
+    // strncpy(tmp, last_location, 100);
+    // tmp[100] = 0;
+    // printf("Struct: \n\t%s\n", tmp);
+
+    token_t token;
+    bool struct_end = false;
+
+    token = next_token(data);
+    // NOTE(erick): named struct
+    if(token.token_type == TK_ID) {
+        printf("Struct name: %.*s\n", (int) token.len, token.token_str);
+        token = next_token(data);
+    }
+
+    expect('{', token);
+
+    while(!struct_end) {
+        token = next_token(data);
+
+        if(token.token_type == '}') { struct_end = true; }
+
+        // NOTE(erick): Sub-struct
+        else if(token.token_type == TK_ID) { parse_declaration(data, true); }
+
+        else if(token.token_type == TK_STRUCT ||
+                token.token_type == TK_UNION) {
+            // TODO(erick): We need a look-around to solve this more cleanly.
+            parse_decl_or_struct_or_union(data, true);
+
+            // TODO(erick): This should not be done by the decl parser
+            // token = next_token(data, token_str);
+            // expect(';', token);
+        } else {
+            fprintf(stderr, "Unexpected token at: %.*s\n", 10, last_location);
+            exit(3);
+        }
+    }
+
+    printf("Out\n");
 }
 
 void parse_file(char* data) {
     token_t token;
-    char token_str[1024];
 
-    while((token = next_token(data, token_str)) != TK_EOF) {
-        switch(token) {
-        case TK_TYPEDEF :
-            printf("TYPEDEF\n");
-            break;
+    while((token = next_token(data)).token_type != TK_EOF) {
+        if(token.token_type == TK_TYPEDEF) {
+            token = next_token(data);
 
-        case TK_STRUCT :
-            printf("STRUCT\n");
-            break;
+            if(token.token_type == TK_STRUCT ||
+               token.token_type == TK_UNION)  {
 
-        case TK_ENUM :
-            printf("ENUM\n");
-            break;
+                parse_struct_or_union(data);
 
-        case TK_ID :
-            printf("ID: %s\n", token_str);
-            break;
+                // Typedef'd name.
+                token = next_token(data);
+                expect(TK_ID, token);
 
-        default:
-            printf("%c\n", token);
+                token = next_token(data);
+                expect(';', token);
+
+
+                // NOTE(erick): We will probably want to parse enums. But not for now.
+                // else if(token == TK_ENUM) { parse_enum(data, token_str); }
+
+                // Nothing to do. We will search for the ';'
+            } else {
+                while(token.token_type != ';' && token.token_type != TK_EOF) {
+                    token = next_token(data);
+                }
+            }
         }
-
     }
 }
 
